1d99741d3892ef82ede2e078cc556d61874e984b
Ronit Goel <110287876+ronitgoel@users.noreply.github.com>
2023-12-29 18:05:45 +0530

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -1,9 +1,9 @@
 // Game Constants & Variables
 let inputDir = {x: 0, y: 0}; 
-const foodSound = new Audio('/food.mp3');
-const gameOverSound = new Audio('/gameover.mp3');
-const moveSound = new Audio('/move.mp3');
-const musicSound = new Audio('/music.mp3');
+const foodSound = new Audio('food.mp3');
+const gameOverSound = new Audio('gameover.mp3');
+const moveSound = new Audio('move.mp3');
+const musicSound = new Audio('music.mp3');
 let speed = 10;
 let score = 0;
 let lastPaintTime = 0;
